f092c55d0dfc57fdd783e606227ec7a4b7d25f69
Bertrand Otoniel <gamerbertrand@gmail.com>
2025-04-26 21:02:56 -0300

    Adicionado a função de média global

--- Escola/models/nota.py
+++ Escola/models/nota.py
@@ -19,17 +19,24 @@ class nota:
         return media
 
     def boletim_completo(self):
-        self.media_historia = self.media_materia(self.nota_historia)
-        self.media_matematica = self.media_materia(self.nota_matematica)
-        self.media_portugues = self.media_materia(self.nota_portugues)
+        self.media_historia = self.media_materia(self.nota_historia)# Arrumar para não repetir isso tanto vezes
+        self.media_matematica = self.media_materia(self.nota_matematica)# Arrumar para não repetir isso tanto vezes
+        self.media_portugues = self.media_materia(self.nota_portugues)# Arrumar para não repetir isso tanto vezes
         print("\n\t\tBoletim Completo\n\n","Matéria\tNotas por bimestre\tMédia\n","Matemática\t",self.nota_matematica,f"\t{(round(self.media_matematica,2))}","\n","Português\t",self.nota_portugues,f"\t{(round(self.media_portugues,2))}""\n","História\t",self.nota_historia,f"\t{(round(self.media_historia,2))}")
 
     def exibe_medias(self):
-        self.media_historia = self.media_materia(self.nota_historia)
-        self.media_matematica = self.media_materia(self.nota_matematica)
-        self.media_portugues = self.media_materia(self.nota_portugues)
-        print(f"\nMédia em matemática: {round(self.media_matematica,2)}\nMédia em português: {round(self.media_portugues)}\nMédia em história: {round(self.media_historia,2)}") 
+        self.media_historia = self.media_materia(self.nota_historia)# Arrumar para não repetir isso tanto vezes
+        self.media_matematica = self.media_materia(self.nota_matematica)# Arrumar para não repetir isso tanto vezes
+        self.media_portugues = self.media_materia(self.nota_portugues)# Arrumar para não repetir isso tanto vezes
+        print(f"\nMédia em matemática: {round(self.media_matematica,2)}\nMédia em português: {round(self.media_portugues)}\nMédia em história: {round(self.media_historia,2)}")
 
+    def exibe_aprovaçoes(self):
+        self.media_historia = self.media_materia(self.nota_historia) # Arrumar para não repetir isso tanto vezes
+        self.media_matematica = self.media_materia(self.nota_matematica)# Arrumar para não repetir isso tanto vezes
+        self.media_portugues = self.media_materia(self.nota_portugues)# Arrumar para não repetir isso tanto vezes
+        lista_medias_notas = [self.media_matematica,self.media_portugues,self.media_historia]
+      # for i in lista_medias_notas:
+          #  if i  
 
     
     #def media_global (self): args e kargs ??
